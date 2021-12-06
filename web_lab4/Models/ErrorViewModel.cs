f915be9ccb5a9c5d6ddf486498fc16bc20407c80
kosti <kosti@DESKTOP-3PMOAH1>
2021-12-06 15:37:30 +0200

    Add project files.

---
+++ web_lab4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_lab4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
